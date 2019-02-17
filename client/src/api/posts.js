c42b3b07653d2c717834aed5b549418b53f30d5a
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-16 22:35:25 -0200

    feat: add request methods for post api

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -1,7 +1,44 @@
 import { doRequest } from '.';
 
-export const getPosts = () => doRequest({ url: '/posts' });
+export const getPosts = (id = '') => doRequest({ url: `/posts/${id}` });
+
+export const getPostsByCategory = category => doRequest({
+  url: `/${category}/posts`,
+});
+
+export const addPost = post => doRequest({
+  body: post,
+  method: 'POST',
+  url: '/posts',
+});
+
+export const updatePost = ({ body, id, title } = {}) => doRequest({
+  body: { body, title },
+  method: 'PUT',
+  url: `/posts/${id}`,
+});
+
+export const removePost = id => doRequest({
+  url: `/posts/${id}`,
+  method: 'DELETE',
+});
+
+const votePost = option => id => doRequest({
+  body: { option },
+  method: 'POST',
+  url: `/posts/${id}`,
+});
+
+export const upVotePost = votePost('upVote');
+
+export const downVotePost = votePost('downVote');
 
 export default {
+  addPost,
   getPosts,
+  getPostsByCategory,
+  updatePost,
+  removePost,
+  upVotePost,
+  downVotePost,
 };
