4b483373d5db90554130e418ded87d27f6be935e
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-17 22:41:58 -0300

    refactor: create post summary

---
+++ client/src/components/Post/Summary/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Feed } from 'semantic-ui-react';
+
+const Summary = ({
+  author,
+  timestamp,
+  title,
+}) => (
+  <Feed.Summary>
+    <Feed.User>{author}</Feed.User>
+    {' '}
+    {title}
+    {' '}
+    <Feed.Date>{new Date(timestamp).toLocaleString()}</Feed.Date>
+  </Feed.Summary>
+);
+
+Summary.propTypes = {
+  author: PropTypes.string.isRequired,
+  timestamp: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Summary;
