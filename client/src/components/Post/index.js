37a30263b42ecc269399b18e901b54a6b65d539b
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-17 22:42:15 -0300

    refactor: use parts of post

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -1,47 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Feed } from 'semantic-ui-react';
+import Summary from './Summary';
+import Meta from './Meta';
 
 const Post = ({
-  author, body, category, timestamp, title,
+  body, ...props
 }) => (
   <Feed.Event>
-    <Feed.Label icon="user circle outline" />
     <Feed.Content>
-      <Feed.Summary>
-        <Feed.User>{author}</Feed.User>
-        {' '}
-        {title}
-        {' '}
-        <Feed.Date>{timestamp}</Feed.Date>
-      </Feed.Summary>
+      <Summary {...props} />
+
       <Feed.Extra text>{body}</Feed.Extra>
-      <Feed.Meta>
-        <Feed.Like icon="thumbs up outline" />
-        <Feed.Like icon="thumbs down outline" />
-        {category
-          && (
-          <Feed.Like>
-              #
-            {category}
-          </Feed.Like>
-          )
-        }
-      </Feed.Meta>
+
+      <Meta {...props} />
     </Feed.Content>
   </Feed.Event>
 );
 
 Post.propTypes = {
-  author: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
-  category: PropTypes.string,
-  timestamp: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
 };
 
-Post.defaultProps = {
-  category: '',
-};
 
 export default Post;
