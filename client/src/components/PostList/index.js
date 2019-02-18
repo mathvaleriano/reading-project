2d7a04e481d6d59e83fcb37940a6c99cf36f483b
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-17 22:42:36 -0300

    feat: connect postlist to redux

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Feed } from 'semantic-ui-react';
 import Post from '../Post';
+import {
+  getPosts as getPostsAction,
+} from '../../store/posts/actions';
 
-const PostList = ({ posts = [] }) => (
-  <Feed>
-    {
-      posts.map(post => <Post {...post} />)
-    }
-  </Feed>
-);
+const PostList = ({ getPosts, posts = [] }) => {
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <Feed>
+      { posts.length
+        ? posts.map(post => <Post {...post} key={post.id} />)
+        : 'No posts found'
+      }
+    </Feed>
+  );
+};
 
 PostList.propTypes = {
+  getPosts: PropTypes.func.isRequired,
   posts: PropTypes.arrayOf(
     PropTypes.shape({
       author: PropTypes.string.isRequired,
       body: PropTypes.string.isRequired,
       category: PropTypes.string,
-      timestamp: PropTypes.string.isRequired,
+      commentCount: PropTypes.number,
+      timestamp: PropTypes.number.isRequired,
       title: PropTypes.string.isRequired,
     }),
   ),
@@ -27,4 +40,15 @@ PostList.defaultProps = {
   posts: [],
 };
 
-export default PostList;
+const mapStateToProps = ({ posts: { posts } }) => ({
+  posts,
+});
+
+const mapDispatchToProps = {
+  getPosts: getPostsAction,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PostList);
