615ed0a5616663b4c54ae1f2d3c22c92ef8843d9
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-17 22:43:46 -0300

    refactor: use posts add and post list on app

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import Provider from '../Provider';
+import { Provider } from 'react-redux';
+import store from '../../store';
+import PostAdd from '../PostAdd';
+import PostList from '../PostList';
 
 const App = () => (
-  <Provider>
-    <div />
+  <Provider store={store}>
+    <PostAdd />
+    <PostList />
   </Provider>
 );
 
