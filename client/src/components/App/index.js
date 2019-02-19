4a368c4b1150bcb50f12c7f5c0f0892f82ed9356
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-18 21:48:08 -0300

    feat: align items on center

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Container } from 'semantic-ui-react';
 import store from '../../store';
 import PostAdd from '../PostAdd';
 import PostList from '../PostList';
 
 const App = () => (
   <Provider store={store}>
-    <PostAdd />
-    <PostList />
+    <Container textAlign="center">
+      <PostAdd />
+      <PostList />
+    </Container>
   </Provider>
 );
 
