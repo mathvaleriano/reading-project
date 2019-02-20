26f1f5f74f3702bc2ce6ecb1b620aa4d6075e366
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-19 22:49:38 -0300

    feat: add postModal

--- client/src/components/PostModal/index.js
+++ client/src/components/PostModal/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import { Modal } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Modal, Feed } from 'semantic-ui-react';
 import Post from '../Post';
 import CommentList from '../CommentList';
+import {
+  setCurrentPost,
+} from '../../store/posts/actions';
 
-
-const PostModal = ({ post }) => (
-  <Modal size="tiny">
-    <Modal.Header>Profile Picture</Modal.Header>
-    <Modal.Content scrolling>
-      <Post {...post} />
-      <CommentList postId={post} />
-    </Modal.Content>
+const PostModal = ({ currentPost, onClose }) => (
+  <Modal
+    open={currentPost}
+    onClose={() => onClose()}
+    size="tiny"
+  >
+    { currentPost
+      && (
+      <Modal.Content scrolling>
+        <Feed>
+          <Post {...currentPost} />
+        </Feed>
+        <CommentList postId={currentPost.id} />
+      </Modal.Content>
+      )
+    }
   </Modal>
 );
 
-export default PostModal;
+PostModal.propTypes = {
+  currentPost: PropTypes.shape({
+    author: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    category: PropTypes.string,
+    commentCount: PropTypes.number,
+    timestamp: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+  }),
+  onClose: PropTypes.func.isRequired,
+};
+
+PostModal.defaultProps = {
+  currentPost: undefined,
+};
+
+const mapStateToProps = ({ posts: { currentPost } }) => ({
+  currentPost,
+});
+
+const mapDispatchToProps = {
+  onClose: setCurrentPost,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostModal);
