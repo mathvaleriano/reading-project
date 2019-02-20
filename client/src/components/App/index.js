db8e4c5ca873153e58fc9b9f933ebd98d77c08a1
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-19 22:50:01 -0300

    feat: include postModal on Root component

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -4,12 +4,14 @@ import { Container } from 'semantic-ui-react';
 import store from '../../store';
 import PostAdd from '../PostAdd';
 import PostList from '../PostList';
+import PostModal from '../PostModal';
 
 const App = () => (
   <Provider store={store}>
     <Container textAlign="center">
       <PostAdd />
       <PostList />
+      <PostModal />
     </Container>
   </Provider>
 );
