3439ab20f79146d248477f66e2cd4aaa1f33d5ab
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-20 21:03:10 -0300

    feat: add undo remove post

--- client/src/store/posts/types.js
+++ client/src/store/posts/types.js
@@ -1,7 +1,8 @@
 export const SET_ERRORS = 'SET_ERRORS';
-export const IS_FETCHING = 'IS_FETCHING';
+export const IS_FETCHING_POSTS = 'IS_FETCHING_POSTS';
 export const SET_POSTS = 'SET_POSTS';
 export const ADD_POST = 'ADD_POST';
 export const REMOVE_POST = 'REMOVE_POST';
+export const UNDO_REMOVE_POST = 'UNDO_REMOVE_POST';
 export const UPDATE_POST = 'UPDATE_POST';
 export const SET_CURRENT_POST = 'SET_CURRENT_POST';
