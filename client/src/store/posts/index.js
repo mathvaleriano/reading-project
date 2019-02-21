785f74eb9c69a06716a36fd2c752b38feab0c7e7
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-20 21:02:57 -0300

    refactor: create aux methods

--- client/src/store/posts/index.js
+++ client/src/store/posts/index.js
@@ -1,18 +1,33 @@
 import initialState from './state';
 import {
   SET_ERRORS,
-  IS_FETCHING,
+  IS_FETCHING_POSTS,
   SET_POSTS,
   ADD_POST,
   REMOVE_POST,
+  UNDO_REMOVE_POST,
   UPDATE_POST,
   SET_CURRENT_POST,
 } from './types';
 
+const toggleDeletedPost = isRemoving => ({ posts = [], id }) => posts.map(
+  post => (post.id === id ? { ...post, deleted: isRemoving } : post),
+);
+
+const removePost = toggleDeletedPost(true);
+const undoRemovePost = toggleDeletedPost(false);
+
+const updatePost = ({ posts = [], newValues }) => posts.map(
+  post => (post.id === newValues.id
+    ? { ...post, ...newValues }
+    : post
+  ),
+);
+
 const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case SET_ERRORS:
-    case IS_FETCHING:
+    case IS_FETCHING_POSTS:
     case SET_POSTS:
     case SET_CURRENT_POST:
       return { ...state, ...payload };
@@ -21,16 +36,17 @@ const reducer = (state = initialState, { type, payload }) => {
     case REMOVE_POST:
       return {
         ...state,
-        posts: state.posts.filter(post => post.id !== payload),
+        posts: removePost({ posts: state.posts, id: payload }),
+      };
+    case UNDO_REMOVE_POST:
+      return {
+        ...state,
+        posts: undoRemovePost({ posts: state.posts, id: payload }),
       };
     case UPDATE_POST:
       return {
         ...state,
-        posts: state.posts.map(post => (
-          post.id === payload.id
-            ? { ...post, ...payload }
-            : post
-        )),
+        posts: updatePost({ posts: state.posts, newValues: payload }),
       };
     default:
       return state;
