2145219e54799951c99db7c37b66a29e1d1b59c8
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-23 17:51:06 -0300

    feat: create component CommentAdd

---
+++ client/src/components/CommentAdd/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Form as SUIForm } from 'semantic-ui-react';
+import Form from '../Form';
+import { handleAddComment } from '../../store/comments/actions';
+
+const CommentAdd = ({ onSubmit, postId }) => (
+  <Form onSubmit={onSubmit}>
+    <input
+      name="parentId"
+      type="hidden"
+      value={postId}
+    />
+
+    <SUIForm.TextArea
+      name="body"
+      placeholder="Type a comment..."
+      required
+    />
+  </Form>
+);
+
+CommentAdd.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  postId: PropTypes.string.isRequired,
+};
+
+const mapDispatchToProps = {
+  onSubmit: handleAddComment,
+};
+
+export default connect(null, mapDispatchToProps)(CommentAdd);
