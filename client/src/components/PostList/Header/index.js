a889b9579b15ceb0084d8ef46a6373d7233ad8c2
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-27 23:21:23 -0300

    feat: add category filter
    Resolve: #1

--- client/src/components/PostList/Header/index.js
+++ client/src/components/PostList/Header/index.js
@@ -1,22 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Container, Dropdown, Icon } from 'semantic-ui-react';
+import {
+  getCategories as getCategoriesAction,
+  setCurrentCategory,
+} from '../../../store/categories/actions';
 
 const orders = [
   { key: 'timestamp', value: 'timestamp', text: 'Timestamp' },
   { key: 'commentCount', value: 'commentCount', text: 'With more comments' },
 ];
 
-const Header = () => (
-  <Container textAlign="right">
-    <Icon name="unordered list" />
-      Order by
-    {' '}
-    <Dropdown
-      inline
-      options={orders}
-      defaultValue={orders[0].value}
-    />
-  </Container>
-);
+const Header = ({
+  categories = [],
+  currentCategory,
+  getCategories,
+  onChangeCategory,
+}) => {
+  useEffect(() => {
+    getCategories();
+  }, []);
 
-export default Header;
+  const categoryOptions = categories.map(
+    ({ name, path }) => ({ key: path, text: name, value: path }),
+  );
+
+  return (
+    <Container textAlign="right">
+      <Icon name="filter" />
+        Order by
+      {' '}
+      <Dropdown
+        inline
+        value={currentCategory}
+        options={categoryOptions}
+        onChange={(e, { value }) => onChangeCategory(value)}
+      />
+      <Icon name="unordered list" />
+        Order by
+      {' '}
+      <Dropdown
+        inline
+        options={orders}
+        defaultValue={orders[0].value}
+      />
+    </Container>
+  );
+};
+
+Header.propTypes = {
+  categories: PropTypes.array,
+  currentCategory: PropTypes.string,
+  getCategories: PropTypes.func.isRequired,
+  onChangeCategory: PropTypes.func.isRequired,
+};
+
+Header.defaultProps = {
+  categories: [],
+  currentCategory: '',
+};
+
+const mapStateToProps = ({
+  categories: { list, currentCategory },
+}) => ({
+  categories: list,
+  currentCategory,
+});
+
+const mapDispatchToProps = {
+  getCategories: getCategoriesAction,
+  onChangeCategory: setCurrentCategory,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
