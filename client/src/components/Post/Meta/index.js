db184aff0a318c7cb047daf2b891201975135acc
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-28 21:26:55 -0300

    feat: implements onclick to up or down vote of a post

--- client/src/components/Post/Meta/index.js
+++ client/src/components/Post/Meta/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { memo } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Feed, Icon } from 'semantic-ui-react';
+import {
+  handleUpVote,
+  handleDownVote,
+} from '../../../store/posts/actions';
 
-const Meta = ({
-  category, commentCount, onClickComments,
+const Meta = memo(({
+  category,
+  commentCount,
+  id,
+  onClickComments,
+  onClickDownVote,
+  onClickUpVote,
+  voteScore,
 }) => (
   <Feed.Meta>
-    <Feed.Like icon="thumbs up outline" />
-    <Feed.Like icon="thumbs down outline" />
+    <Feed.Like
+      icon="thumbs up outline"
+      onClick={() => onClickUpVote(id)}
+    />
+    <Feed.Like
+      icon="thumbs down outline"
+      onClick={() => onClickDownVote(id)}
+    />
+    Vote Score:
+    {' '}
+    { voteScore }
     <Feed.Like onClick={onClickComments}>
       <Icon name="comments outline" />
       {commentCount > 0 && commentCount}
@@ -19,18 +39,31 @@ const Meta = ({
     </Feed.Like>
     )}
   </Feed.Meta>
-);
+));
 
 Meta.propTypes = {
   category: PropTypes.string,
   commentCount: PropTypes.number,
+  id: PropTypes.string.isRequired,
   onClickComments: PropTypes.func,
+  onClickDownVote: PropTypes.func.isRequired,
+  onClickUpVote: PropTypes.func.isRequired,
+  voteScore: PropTypes.number,
 };
 
 Meta.defaultProps = {
   category: '',
   commentCount: 0,
   onClickComments: () => {},
+  voteScore: 0,
 };
 
-export default Meta;
+const mapDispatchToProps = {
+  onClickDownVote: handleDownVote,
+  onClickUpVote: handleUpVote,
+};
+
+export default connect(
+  undefined,
+  mapDispatchToProps,
+)(Meta);
