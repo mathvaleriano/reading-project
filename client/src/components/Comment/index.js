f26810724c18352507be00463649c6acf75b7883
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-28 22:44:18 -0300

    feat: use up and down vote on component

--- client/src/components/Comment/index.js
+++ client/src/components/Comment/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Comment as SUIComment, Icon } from 'semantic-ui-react';
+import {
+  handleUpVote,
+  handleDownVote,
+} from '../../store/comments/actions';
 
-const Comment = ({
+const Comment = memo(({
   author,
   body,
   id,
+  onClickDownVote,
+  onClickUpVote,
   timestamp,
   voteScore = 0,
 }) => (
@@ -20,28 +27,30 @@ const Comment = ({
       <SUIComment.Text>{body}</SUIComment.Text>
 
       <SUIComment.Actions>
-        <SUIComment.Action>
-          Score
-          {' '}
-          {voteScore}
-        </SUIComment.Action>
-
-        <SUIComment.Action>
+        <SUIComment.Action onClick={() => onClickUpVote(id)}>
           <Icon name="thumbs up outline" />
         </SUIComment.Action>
 
-        <SUIComment.Action>
+        <SUIComment.Action onClick={() => onClickDownVote(id)}>
           <Icon name="thumbs down outline" />
+        </SUIComment.Action>
+
+        <SUIComment.Action>
+          Vote Score:
+          {' '}
+          {voteScore}
         </SUIComment.Action>
       </SUIComment.Actions>
     </SUIComment.Content>
   </SUIComment>
-);
+));
 
 Comment.propTypes = {
   id: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
+  onClickDownVote: PropTypes.func.isRequired,
+  onClickUpVote: PropTypes.func.isRequired,
   timestamp: PropTypes.number.isRequired,
   voteScore: PropTypes.number,
 };
@@ -50,4 +59,12 @@ Comment.defaultProps = {
   voteScore: 0,
 };
 
-export default Comment;
+const mapDispatchToProps = {
+  onClickDownVote: handleDownVote,
+  onClickUpVote: handleUpVote,
+};
+
+export default connect(
+  undefined,
+  mapDispatchToProps,
+)(Comment);
