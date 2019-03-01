c3255d6923710d23c202812562105f5543d0f03a
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-28 22:44:02 -0300

    feat: insert up and down function into reducer

--- client/src/store/comments/index.js
+++ client/src/store/comments/index.js
@@ -7,21 +7,42 @@ import {
   REMOVE_COMMENT,
   UNDO_REMOVE_COMMENT,
   UPDATE_COMMENT,
+  UP_VOTE_COMMENT,
+  DOWN_VOTE_COMMENT,
 } from './types';
 
-const toggleDeletedComment = isRemoving => ({ comments = [], id }) => comments.map(
-  comment => (comment.id === id ? { ...comment, deleted: isRemoving } : comment),
+const toggleDeletedComment = isRemoving => ({
+  comments = [],
+  id,
+}) => comments.map(
+  comment => (
+    comment.id === id
+      ? { ...comment, deleted: isRemoving }
+      : comment
+  ),
 );
 
 const removeComment = toggleDeletedComment(true);
 const undoRemoveComment = toggleDeletedComment(false);
 
-const updateComment = ({ comments = [], newValues }) => comments.map(
+const updateComment = ({
+  comments = [],
+  newValues,
+}) => comments.map(
   comment => (comment.id === newValues.id
     ? { ...comment, ...newValues }
     : comment
   ),
 );
+
+const toggleVote = ({ commentId, voteScoreModifier }) => comment => (
+  comment.id === commentId
+    ? { ...comment, voteScore: comment.voteScore + voteScoreModifier }
+    : comment
+);
+
+const upVote = commentId => toggleVote({ commentId, voteScoreModifier: 1 });
+const downVote = commentId => toggleVote({ commentId, voteScoreModifier: -1 });
 
 const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
@@ -40,17 +61,36 @@ const reducer = (state = initialState, { type, payload }) => {
     case REMOVE_COMMENT:
       return {
         ...state,
-        comments: removeComment({ comments: state.comments, id: payload }),
+        comments: removeComment({
+          comments: state.comments,
+          id: payload,
+        }),
       };
     case UNDO_REMOVE_COMMENT:
       return {
         ...state,
-        comments: undoRemoveComment({ comments: state.comments, id: payload }),
+        comments: undoRemoveComment({
+          comments: state.comments,
+          id: payload,
+        }),
       };
     case UPDATE_COMMENT:
       return {
         ...state,
-        comments: updateComment({ comments: state.comments, newValues: payload }),
+        comments: updateComment({
+          comments: state.comments,
+          newValues: payload,
+        }),
+      };
+    case UP_VOTE_COMMENT:
+      return {
+        ...state,
+        comments: state.comments.map(upVote(payload.id)),
+      };
+    case DOWN_VOTE_COMMENT:
+      return {
+        ...state,
+        comments: state.comments.map(downVote(payload.id)),
       };
     default:
       return state;
