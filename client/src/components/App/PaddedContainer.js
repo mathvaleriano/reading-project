925b69c64da714c220767a588432acd6fd0d5a9d
Matheus Valeriano <math.valeriano@gmail.com>
2019-03-02 12:54:15 -0300

    feat: create padded container for app

---
+++ client/src/components/App/PaddedContainer.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Container } from 'semantic-ui-react';
+
+const PaddedContainer = styled(Container)`
+  padding-top: 2.5vw;
+  padding-bottom: .5vh;
+`;
+
+export default PaddedContainer;
