f9e929a8629519b6603ff2f8acb0f8579dfa276f
Matheus Valeriano <math.valeriano@gmail.com>
2019-03-02 12:57:44 -0300

    feat: include order module into the store

--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import categories from './categories';
 import comments from './comments';
+import order from './order';
 import posts from './posts';
 
 export default combineReducers({
   categories,
   comments,
+  order,
   posts,
 });
