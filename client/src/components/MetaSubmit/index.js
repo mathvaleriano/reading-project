42df9c9caa5f270c483a9723f88904a866150bf3
Matheus Valeriano <math.valeriano@gmail.com>
2019-03-02 15:58:06 -0300

    feat: create meta submit component

---
+++ client/src/components/MetaSubmit/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const MetaSubmit = styled.button`
+background-color: transparent;
+border: none;
+color: #173F5F;
+cursor: pointer;
+transition: .2s color ease
+font-size: .85714286em;
+&:hover {
+  color: #20649B;
+}
+`;
+
+export default MetaSubmit;
